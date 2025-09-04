f6d65dd27d2101ea7ce3ec0d9a632c01620d51cf
CX330 <quanghuy11102k4@gmail.com>
2025-09-04 23:39:56 +0700

    update

--- src/Collection.kt
+++ src/Collection.kt
@@ -1,40 +1,63 @@
-fun listExample() {
-    val items = listOf("apple", "banana", "kiwi")
-    for (item in items) {
-        println(item)
-    }
-    items.forEach { item -> println(item) }
-    items.forEach { println(it) }
-    val fruits = listOf("banana", "avocado", "apple", "kiwi")
-    fruits
-        .filter { it.startsWith("a") }
-        .sortedBy { it }
-        .map { it.uppercase() }
-        .forEach { println(it) }
-}
-
-fun setExample() {
-    val numbers = setOf(1, 2, 3, 4, 5, 5, 4)
-    println(numbers)
-    val fruits = listOf("banana", "avocado", "apple", "kiwi", "apple")
-    val fruitSet = fruits.toSet()
-    println(fruitSet)
-}
-
-fun mapExample() {
-    val numbers = mapOf("one" to 1, "two" to 2, "three" to 3)
-    for ((key, value) in numbers) {
-        println("$key = $value")
-    }
-    numbers.forEach { (key, value) -> println("$key = $value") }
-    numbers.forEach { println("${it.key} = ${it.value}") }
-    val fruits = listOf("banana", "avocado", "apple", "kiwi", "apple")
-    val fruitCount = fruits.groupingBy { it }.eachCount()
-    println(fruitCount)
-}
-
 fun main() {
     listExample()
     setExample()
     mapExample()
+}
+fun listExample() {
+    val items = listOf("apple", "banana", "mango")
+    for (item in items) {
+        print("$item ")
+    }
+    println()
+    items.forEach { item -> print("$item ")}
+    println()
+    items.forEach { print("$it ") }
+    println()
+    val fruits = listOf("banana", "avocado", "apple", "mango")
+    fruits
+        .filter { it.startsWith("a") }
+        .sortedBy { it }
+        .map { it.uppercase() }
+        .forEach { print("$it ") }
+    println()
+    fruits.reduce { acc, s -> "$acc, $s" }
+    val num = listOf(1, 2, 3, 4, 5)
+    println(num.all { it > 0 })
+    println(num.any { it > 3 })
+    println(num.count { it > 3 })
+    println(num.find { it > 3 })
+    println(num.findLast { it > 3 })
+    println(num.maxOrNull())
+    println(num.minOrNull())
+    println(num.sum())
+    println(num.fold(10) { acc, i -> acc + i })
+}
+
+fun setExample() {
+    val num = setOf(1, 2, 3, 4, 5, 5, 4)
+    println(num)
+    val fruits = listOf("banana", "avocado", "apple", "mango", "apple")
+    val fruitSet = fruits.toSet()
+    println(fruitSet)
+    val fruitSet2 = fruits.toSortedSet()
+    println(fruitSet2)
+    val num2 = setOf(3, 4, 5, 6, 7)
+    println(num.intersect(num2))
+    println(num.union(num2))
+    println(num.subtract(num2))
+    println(num2.subtract(num))
+}
+
+fun mapExample() {
+    val num = mapOf("one" to 1, "two" to 2, "three" to 3)
+    for ((key, value) in num) {
+        println("$key = $value")
+    }
+    num.forEach { (key, value) -> println("$key = $value") }
+    num.forEach { println("${it.key} = ${it.value}") }
+    val fruits = listOf("banana", "avocado", "apple", "mango", "apple")
+    val fruitCount = fruits.groupingBy { it }.eachCount()
+    println(fruitCount)
+    val fruitCount2 = fruits.groupBy { it.startsWith("a") }
+    println(fruitCount2)
 }
--- src/LuaChonMenu.kt
+++
@@ -1,8 +0,0 @@
-enum class LuaChonMenu(val giaTri: Int) {
-    THEM_SINH_VIEN(1),
-    HIEN_THI_DANH_SACH(2),
-    TIM_KIEM_SINH_VIEN(3),
-    XOA_SINH_VIEN(4),
-    CAP_NHAT_SINH_VIEN(5),
-    THOAT(6)
-}
